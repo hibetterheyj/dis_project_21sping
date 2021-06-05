542ae587c834dee57dba88eaaf3771e3d9206e62
tha-santacruz <83542144+tha-santacruz@users.noreply.github.com>
2021-06-05 10:02:35 +0200

    error metric plot

--- Matlab/supervisor_logs.m
+++ Matlab/supervisor_logs.m
@@ -1,32 +1,40 @@
 clear all
 close all
 clc
+%set(groot,'DefaultAxesFontSize', 14)
+set(groot,'DefaultLineLineWidth', 1.5)
 
 fid= fopen('../Initial_Material/controllers/localization_supervisor/errors.csv');
-data= textscan(fid,"%f %f %f %f %f %f %f %f %f","Delimiter",";","headerlines",1);
+data= textscan(fid,"%f %f %f %f %f %f %f %f %f %f %f","Delimiter",";","headerlines",1);
 
 time= data{:,1};
 gps_error= data{:,2};
 acc_error= data{:,3};
 enc_error= data{:,4};
-kal_error= data{:,5};
-gps_mean= data{:,6};
-acc_mean= data{:,7};
-% enc_mean= data{:,8};
-% kal_mean= data{:,9};
+kal1_error= data{:,5};
+kal2_error= data{:,6};
+gps_mean= data{:,7};
+acc_mean= data{:,8};
+enc_mean= data{:,9};
+kal1_mean= data{:,10};
+kal2_mean= data{:,11};
 
 figure("name","ERRORS")
+title("Localization Error");
 hold on
 % plot(time,gps_error,"displayname","gps error");
 % plot(time,acc_error,"displayname","acc error");
 % plot(time,enc_error,"displayname","enc error");
 % plot(time,kal_error,"displayname","kal error");
 
-plot(gps_error,"displayname","gps error");
-plot(acc_error,"displayname","acc error");
-plot(enc_error,"displayname","enc error");
-plot(kal_error,"displayname","kal error");
-
+plot(time,gps_error,"displayname","gps error");
+plot(time,acc_error,"displayname","acc error");
+plot(time,enc_error,"displayname","enc error");
+plot(time,kal1_error,"displayname","kal acc error");
+plot(time,kal2_error,"displayname","kal enc error");
+xlabel("time (s)")
+ylabel("error metric value")
 legend show
+legend("location","northwest")
 
 fclose(fid);
